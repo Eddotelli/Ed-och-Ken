354ea927e0132ea13efa25737598f3b89d8f8b07
Edrisa Jallow <edrisa.j98@hotmail.com>
2024-09-16 14:25:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ed_och_Ken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YOOOOOO KEN!!!!");
+            Console.WriteLine("What's poppin my duuuude^^");
+        }
+    }
+}
