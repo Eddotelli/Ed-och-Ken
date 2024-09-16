5a1b1863e90ab5cb880e4b0634986cd0af6b5bbd
audioken <kenzachrisson@gmail.com>
2024-09-16 15:06:46 +0200

    Testar ny rad

--- Program.cs
+++ Program.cs
@@ -4,12 +4,15 @@
     {
         static void Main(string[] args)
         {
+            //Eddie's kommentar
             Console.WriteLine("YOOOOOO KEN!!!!");
             Console.WriteLine("What's poppin my duuuude^^");
 
+            //Ken's kommentar
             Console.WriteLine("Ey det är maxfett!");
             Console.WriteLine("Själv då kingo?");
 
+            //Eddie's kommentar
             Console.WriteLine("All good in the hood bruv!");
         }
     }
